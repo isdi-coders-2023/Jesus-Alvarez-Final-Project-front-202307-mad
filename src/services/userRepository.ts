4b532ce9d97e3be5a0f953953e048fa49c20f985
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-09-14 12:47:11 +0200

    Add login() method to userRepository

--- src/services/userRepository.ts
+++ src/services/userRepository.ts
@@ -1,4 +1,4 @@
-import { User, UserNoId } from '../model/user';
+import { User, UserLoginData, UserNoId } from '../model/user';
 
 export class ApiUserRepository {
   urlBase: string;
@@ -16,6 +16,16 @@ export class ApiUserRepository {
     });
     if (!response.ok)
       throw new Error(`Error ${response.status}: ${response.statusText}`);
+    const data = await response.json();
+    return data;
+  }
+
+  async login(item: UserLoginData): Promise<User> {
+    const response = await fetch(`${this.urlBase}/login`, {
+      method: 'PATCH',
+      body: JSON.stringify(item),
+      headers: { 'Content-Type': 'application/json' },
+    });
     const data = await response.json();
     return data;
   }
