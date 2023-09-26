315ca902949527d3ad1e619058c02bee5a8eacf9
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-09-26 13:45:12 +0200

    Change LoginPage test

--- src/components/register/register.test.tsx
+++ src/components/register/register.test.tsx
@@ -9,20 +9,20 @@ import { Register } from './register';
 jest.mock('../../hooks/use-users');
 
 describe('Given the component Register', () => {
+  const mockNavigate = jest.fn();
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => ({
+      navigate: mockNavigate,
+    }),
+  }));
   describe('When it is rendered', () => {
-    const mockNavigate = jest.fn();
-    jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'),
-      useNavigate: () => ({
-        navigate: mockNavigate,
-      }),
-    }));
     const changedState = {
       registerStatus: 'registered',
     };
     (useUsers as jest.Mock).mockReturnValue({
       usersRegister: jest.fn().mockResolvedValue(changedState),
-      usersState: { registerStatus: 'registered' },
+      registerStatus: 'registered',
     });
     beforeEach(() => {
       render(
@@ -42,7 +42,29 @@ describe('Given the component Register', () => {
       const formElement = screen.getByRole('form');
       await fireEvent.submit(formElement);
       expect(useUsers().usersRegister).toHaveBeenCalled();
-      expect(changedState.registerStatus).toEqual('registered');
+    });
+  });
+  describe('When it is rendered with an error in the register', () => {
+    const changedState = {
+      registerStatus: 'registered',
+    };
+    (useUsers as jest.Mock).mockReturnValue({
+      usersRegister: jest.fn().mockResolvedValue(changedState),
+      registerStatus: 'error',
+    });
+    beforeEach(() => {
+      render(
+        <MemoryRouter>
+          <Provider store={tennisZoneStore}>
+            <Register></Register>
+          </Provider>
+        </MemoryRouter>
+      );
+    });
+    test('dfd', async () => {
+      const formElement = screen.getByRole('form');
+      await fireEvent.submit(formElement);
+      expect(useUsers().usersRegister).toHaveBeenCalled();
     });
   });
 });
